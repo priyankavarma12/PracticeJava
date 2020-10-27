05a74a351638cf563000e6e32463f4ab64372ed8
priyanka_varma <priyankavarma717@gmail.com>
2020-10-27 17:11:37 +0530

    Update star pattern

---
+++ src/pattern/Pattern20.java
@@ -0,0 +1,35 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Pattern20 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner( System.in );
+        int n = scanner.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==1 || j == n){
+                    System.out.print("*\t");
+                } else if(i>n/2 &&  (i==j || i+j == n+1)){
+                    System.out.print("*\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+/**
+ * n is odd
+Pattern20
+*				*
+*				*
+*		*		*
+*	*		*	*
+*				*
+ */
