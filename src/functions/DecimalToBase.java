3cd12e884e17cd237d6cca2e2fbac2d88f4e8921
priyanka_varma <priyankavarma717@gmail.com>
2020-10-28 20:41:07 +0530

    Add Decimal to any Base

---
+++ src/functions/DecimalToBase.java
@@ -0,0 +1,27 @@
+package functions;
+
+import java.util.Scanner;
+
+public class DecimalToBase {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int b = scn.nextInt();
+        int dn = getValueInBase(n, b);
+        System.out.println(dn);
+    }
+
+    public static int getValueInBase(int n, int b){
+        // write code here
+        int rv = 0;
+        int pow = 1; //(10 pow 0 = 1)
+        while(n > 0){
+            int dig = n % b;
+            n = n / b;
+
+            rv = rv + dig * pow;
+            pow = pow * 10;
+        }
+        return rv;
+    }
+}
